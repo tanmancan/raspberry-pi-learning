3c5c778b3549baaae20c6c29e647f7a250b8f672
tanmancan <contact@tkarimdesign.com>
2017-01-15 16:15:47 +0000

    for formatting. Added check to ssee if led is already on

--- user-input/user-input.cpp
+++ user-input/user-input.cpp
@@ -18,32 +18,41 @@ int main(void) {
 
   while(1)
   {
+    int pinStatus = digitalRead(LedPin);
+    std::cout << "Pin status\n" << pinStatus << std::endl;
+
+    std::cout << "===========================\n";
     std::cout << "Turn lights on or off?\n";
     std::cin >> usrinput;
 
-    std::cout << std::endl; << std::endl;;
+    std::cout << std::endl;
 
     std::cout << "You entered...\n";
-    std::cout << usrinput << std::endl;
-
-    std::cout << std::endl; << std::endl;;
+    std::cout << usrinput << std::endl << std::endl;
 
     if (usrinput == "on" || usrinput == "On" || usrinput == "ON")
     {
-      std::cout << "Turning lights on...\n";
-      std::cout << std::endl; << std::endl;;
-      digitalWrite(LedPin, LOW);
+      if (pinStatus == 0)
+      {
+        std::cout << "Led is already on...\n\n";
+      }else {
+        std::cout << "Turning lights on...\n\n";
+        digitalWrite(LedPin, LOW);
+      }
     } else if (usrinput == "off" || usrinput == "Off" || usrinput == "OFF")
     {
-      std::cout << "Turning lights off...\n";
-      std::cout << std::endl; << std::endl;;
-      digitalWrite(LedPin, HIGH);
+      if (pinStatus == 1)
+      {
+        std::cout << "Led is already off...\n\n";
+      }else {
+        std::cout << "Turning lights off...\n\n";
+        digitalWrite(LedPin, HIGH);
+      }
     } else if (usrinput != "on" || usrinput != "off")
     {
-      std::cout << "Please only enter a yes or no response.\n";
-      std::cout << std::endl; << std::endl;;
+      std::cout << "Please only enter a yes or no response.\n\n";
     }
-
+    std::cout << "===========================\n";
   }
 
   return 0;
