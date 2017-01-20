c6572505b69825bc117456f0964d8fddafb91bf1
tanmancan <contact@tkarimdesign.com>
2017-01-20 21:44:01 +0000

    added some instruction for decimal = binary conversion

--- seg-display/seg-display.cpp
+++ seg-display/seg-display.cpp
@@ -17,6 +17,24 @@ int main(void) {
 
   while(1) {
     std::cout << "Enter value...\n";
+    std::cout << "off = 0\n";
+    std::cout << "a = 2\n";
+    std::cout << "b = 4\n";
+    std::cout << "c = 8\n";
+    std::cout << "d = 16\n";
+    std::cout << "e = 32\n";
+    std::cout << "f = 64\n";
+    std::cout << "g = 128\n";
+    std::cout << "--------\n";
+    std::cout << "a+ = 1\n";
+    std::cout << "b+ = 3\n";
+    std::cout << "c+ = 7\n";
+    std::cout << "d+ = 15\n";
+    std::cout << "e+ = 31\n";
+    std::cout << "f+ = 63\n";
+    std::cout << "g+ = 127\n";
+    std::cout << "--------\n";
+
     std::cin >> SegCode;
     std::cout << std::endl;
     std::cout << "You entered...\n";
