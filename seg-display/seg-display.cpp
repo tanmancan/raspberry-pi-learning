3e276b21035df99199c63a6d42d28000260ca739
tanmancan <contact@tkarimdesign.com>
2017-01-20 02:37:32 +0000

    segment display program

--- seg-display/seg-display.cpp
+++ seg-display/seg-display.cpp
@@ -2,6 +2,28 @@
 #include <stdio.h>
 #include <iostream>
 
+long SegCode;
+
 int main(void) {
+  if (wiringPiSetup() < 0)
+  {
+    std::cout << "Wiring Pi did not setup...\n";
+  }
+
+  for (int i = 0; i < 8; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
+
+  while(1) {
+    std::cout << "Enter value...\n";
+    std::cin >> SegCode;
+    std::cout << std::endl;
+    std::cout << "You entered...\n";
+    std::cout << SegCode << std::endl;
+
+    digitalWriteByte(SegCode);
+  }
+
   return 0;
 }
